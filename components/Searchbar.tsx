e15b989d6472bb159719827c7e6f64d17fddf483
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-12-26 19:14:04 -0500

    searchbar

---
+++ components/Searchbar.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+function Searchbar() {
+    const handleSubmit = () => {}
+  return (
+    <form 
+        onSubmit={handleSubmit} 
+        className="flex flex-wrap gap-4 mt-12"
+    >
+        <input 
+        type="text" 
+        placeholder="Enter product link"
+        className="searchbar-input" 
+        />
+
+        <button className="searchbar-btn" type="submit">
+            Search
+        </button>
+    </form>
+  )
+}
+
+export default Searchbar
