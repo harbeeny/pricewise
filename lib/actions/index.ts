80fee645582f5c0c4145cced64846ef98423c811
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-12-28 14:38:20 -0500

    Database finished

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -15,7 +18,30 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
         let product = scrapedProduct;
 
-        //const existingProduct = await Product
+        const existingProduct = await Product.findOne({url: scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory: any =  [
+                ...existingProduct.priceHistory,
+                { price : scrapedProduct.currentPrice }
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory : updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory)
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            {url: scrapedProduct.url},
+            product,
+            { upsert : true, new: true},
+        );
+
+        revalidatePath(`/products/${newProduct._id}`)
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
