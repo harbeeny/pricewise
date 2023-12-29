cde36bd019bdb04ed01b18845e8673773cdf6264
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2023-12-29 14:20:39 -0500

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,7 +7,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAndStoreProduct } from "@/lib/actions";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 5; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
